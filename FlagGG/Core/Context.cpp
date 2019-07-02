3708c6534c91b9e46ed1464a8dfcf939f0b3b208
linmeichen <596922196@qq.com>
2019-07-02 10:23:05 +0800

    windows编译过

--- FlagGG/Core/Context.cpp
+++ FlagGG/Core/Context.cpp
@@ -5,42 +5,42 @@ namespace FlagGG
 	namespace Core
 	{
 #ifdef WIN32
-        template <> void RegisterVariable<int8_t>(int8_t* variable, const Container::String& variableName)
+        template <> void Context::RegisterVariable<int8_t>(int8_t* variable, const Container::String& variableName)
         {
             
         }
 
-        template <> void RegisterVariable<uint8_t>(uint8_t* variable, const Container::String& variableName)
+        template <> void Context::RegisterVariable<uint8_t>(uint8_t* variable, const Container::String& variableName)
         {
 
         }
 
-        template <> void RegisterVariable<int16_t>(int16_t* variable, const Container::String& variableName)
+        template <> void Context::RegisterVariable<int16_t>(int16_t* variable, const Container::String& variableName)
         {
 
         }
 
-        template <> void RegisterVariable<uint16_t>(uint16_t* variable, const Container::String& variableName)
+        template <> void Context::RegisterVariable<uint16_t>(uint16_t* variable, const Container::String& variableName)
         {
 
         }
 
-        template <> void RegisterVariable<int32_t>(int32_t* variable, const Container::String& variableName)
+        template <> void Context::RegisterVariable<int32_t>(int32_t* variable, const Container::String& variableName)
         {
 
         }
 
-        template <> void RegisterVariable<uint32_t>(uint32_t* variable, const Container::String& variableName)
+        template <> void Context::RegisterVariable<uint32_t>(uint32_t* variable, const Container::String& variableName)
         {
 
         }
 
-        template <> void RegisterVariable<int64_t>(int64_t* variable, const Container::String& variableName)
+        template <> void Context::RegisterVariable<int64_t>(int64_t* variable, const Container::String& variableName)
         {
 
         }
 
-        template <> void RegisterVariable<uint64_t>(uint64_t* variable, const Container::String& variableName)
+        template <> void Context::RegisterVariable<uint64_t>(uint64_t* variable, const Container::String& variableName)
         {
 
         }
