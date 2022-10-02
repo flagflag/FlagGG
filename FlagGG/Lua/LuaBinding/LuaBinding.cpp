59ff9e89e38f82d6b568283a95297ffffb3e60bb
linmeichen <596922196@qq.com>
2022-10-02 19:28:01 +0800

    LuaEx

---
+++ FlagGG/Lua/LuaBinding/LuaBinding.cpp
@@ -0,0 +1,183 @@
+#include "Core/CryAssert.h"
+#include "Container/Str.h"
+#include "Container/Vector.h"
+
+#include "LuaBinding.h"
+#include "LuaExtend.h"
+
+#include <cstring>
+
+namespace FlagGG
+{
+	namespace Lua
+	{
+		static void luaex_record_class(lua_State* L, const char* name, const char* base)
+		{
+			// 记录一下继承关系
+			lua_pushliteral(L, "ClassMap");
+			lua_rawget(L, -2);
+			if (!lua_istable(L, -1))
+			{
+				lua_pop(L, 1);
+				lua_newtable(L);
+				lua_pushliteral(L, "ClassMap");
+				lua_pushvalue(L, -2);
+				lua_rawset(L, -4);
+			}
+			lua_pushstring(L, name);
+			lua_pushstring(L, base);
+			lua_rawset(L, -3);
+			lua_pop(L, 1);
+		}
+
+		void luaex_beginclass(lua_State* L, const char* name, const char* base, lua_CFunction contructor, lua_CFunction destructor)
+		{
+			// 获取上下文
+			luaex_pushcontext(L);
+
+			luaex_record_class(L, name, base);
+
+			// 创建class表，然后把他push到context里
+			lua_newtable(L);  // @class_t
+			lua_pushstring(L, name);
+			lua_pushvalue(L, -2);
+			lua_rawset(L, -4);
+
+			// 设置class名
+			lua_pushliteral(L, "__cname");
+			lua_pushstring(L, name);
+			lua_rawset(L, -3);
+
+			// 创建一张空表用来保存元表
+			lua_createtable(L, 1, 0);  // @meta_T
+
+			// gc元表
+			if (destructor)
+			{
+				lua_pushliteral(L, "__gc");
+				lua_pushcfunction(L, destructor);
+				lua_rawset(L, -3);
+			}
+
+			// index元表，后续通过luaex_classfunction往元表里插入class的function
+			lua_pushliteral(L, "__index");
+
+			lua_newtable(L);  // @index_t
+
+			// 继承，把父类元表方法拷过来
+			if (base && strlen(base) > 0)
+			{
+				luaex_pushcontext(L);
+				lua_pushstring(L, base);
+				lua_rawget(L, -2);
+				CRY_ASSERT_MESSAGE(lua_istable(L, -1), "base class is not a table");
+
+				lua_getmetatable(L, -1);
+				lua_pushliteral(L, "__index");
+				lua_rawget(L, -2);
+
+				// 遍历元素，然后拷贝到另一张表中，不要直接用__index，因为是引用关系！！！
+				Container::Vector<Container::String> keys;
+				lua_pushnil(L);
+				while (lua_next(L, -2) != 0)
+				{
+					keys.Push(lua_tostring(L, -2));
+					lua_pop(L, 1);
+				}
+				for (const auto& key : keys)
+				{
+					lua_pushstring(L, key.CString());
+					lua_rawget(L, -2);
+
+					lua_pushstring(L, key.CString());
+					lua_pushvalue(L, -2);
+					lua_rawset(L, -8);
+					lua_pop(L, 1);
+				}
+
+				lua_pop(L, 4);
+			}
+
+			if (contructor)
+			{
+				luaex_classfunction(L, "__create", contructor);
+
+				// 同时把构造函数引用一份作为new函数调用
+				lua_pushliteral(L, "new");
+				lua_pushcfunction(L, contructor);
+				lua_rawset(L, -6);
+			}
+		}
+
+		void luaex_endclass(lua_State* L)
+		{
+			// 把classfunction表插入元表
+			lua_rawset(L, -3);
+
+			// 设置为class表的元表
+			lua_setmetatable(L, -2);
+
+			// 设置class类型为type == 1
+			lua_pushliteral(L, "__ctype");
+			lua_pushinteger(L, 1);
+			lua_rawset(L, -3);
+
+			// 复原堆栈
+			lua_pop(L, 2);
+		}
+
+		void luaex_classfunction(lua_State* L, const char* name, lua_CFunction func)
+		{
+			lua_pushstring(L, name);
+			lua_pushcfunction(L, func);
+			lua_rawset(L, -3);  // -3 is @index_t
+		}
+
+		void luaex_function(lua_State* L, const char* name, lua_CFunction func)
+		{
+			luaex_pushcontext(L);
+			lua_pushstring(L, name);
+			lua_pushcfunction(L, func);
+			lua_rawset(L, -3);
+			lua_pop(L, 1);
+		}
+
+		void luaex_globalfunction(lua_State* L, const char* name, lua_CFunction func)
+		{
+			lua_pushcfunction(L, func);
+			lua_setglobal(L, name);
+		}
+
+		void luaex_constant(lua_State* L, const char* name, long long value)
+		{
+			luaex_pushcontext(L);
+			lua_pushstring(L, name);
+			lua_pushinteger(L, value);
+			lua_rawset(L, -3);
+			lua_pop(L, 1);
+		}
+
+		void luaex_constant(lua_State* L, const char* name, const char* value)
+		{
+			luaex_pushcontext(L);
+			lua_pushstring(L, name);
+			lua_pushstring(L, value);
+			lua_rawset(L, -3);
+			lua_pop(L, 1);
+		}
+
+		void luaex_classconstant(lua_State* L, const char* name, long long value)
+		{
+			lua_pushstring(L, name);
+			lua_pushinteger(L, value);
+			lua_rawset(L, -3);
+		}
+
+		void luaex_classconstant(lua_State* L, const char* name, const Container::String& value)
+		{
+			lua_pushstring(L, name);
+			lua_pushstring(L, value.CString());
+			lua_rawset(L, -3);
+		}
+	}
+}
---
+++ FlagGG/Lua/LuaBinding/LuaExtend.cpp
@@ -0,0 +1,200 @@
+#include "Core/CryAssert.h"
+#include "Container/Str.h"
+#include "Log.h"
+
+#include "LuaExtend.h"
+
+namespace FlagGG
+{
+	namespace Lua
+	{
+		lua_State* luaex_getmainstate(lua_State* L)
+		{
+			lua_rawgeti(L, LUA_REGISTRYINDEX, LUA_RIDX_MAINTHREAD);
+			lua_State* mainState = lua_tothread(L, -1);
+			lua_pop(L, 1);
+			return mainState;
+		}
+
+		void luaex_pushglobaltable(lua_State* L, const char* name, bool keyWeakRef/* = false*/, bool valueWeakRef/* = false*/)
+		{
+			lua_pushstring(L, name);
+			lua_rawget(L, LUA_REGISTRYINDEX);
+			if (lua_isnil(L, -1))
+			{
+				lua_pop(L, 1);
+				lua_newtable(L);
+				if (keyWeakRef || valueWeakRef)
+				{
+					lua_newtable(L);
+					lua_pushliteral(L, "__mode");
+					if (keyWeakRef && !valueWeakRef)
+						lua_pushliteral(L, "k");
+					else if (!keyWeakRef && valueWeakRef)
+						lua_pushliteral(L, "v");
+					else
+						lua_pushliteral(L, "kv");
+					lua_rawset(L, -3);
+					lua_setmetatable(L, -2);
+				}
+				lua_pushstring(L, name);
+				lua_pushvalue(L, -2);
+				lua_rawset(L, LUA_REGISTRYINDEX);
+			}
+		}
+
+		void luaex_pushcontext(lua_State* L)
+		{
+			luaex_pushglobaltable(L, "LuaexContext");
+			CRY_ASSERT_MESSAGE(lua_istable(L, -1), "LuaexContext is not a table");
+		}
+
+		static void luaex_pushusertypetable(lua_State* L)
+		{
+			luaex_pushglobaltable(L, "LuaexUserData", false, true);
+			CRY_ASSERT_MESSAGE(lua_istable(L, -1), "LuaexUserData is not a table");
+		}
+
+		bool luaex_juedge_inherit(lua_State* L, const Container::String& type, const Container::String& superClass)
+		{
+			// 拿出继承关系
+			luaex_pushcontext(L);
+			lua_pushliteral(L, "ClassMap");
+			lua_rawget(L, -2);
+			CRY_ASSERT(lua_istable(L, -1));
+
+			Container::String subClass = type;
+			bool findIt = false;
+			while (!subClass.Empty())
+			{
+				if (subClass == superClass)
+				{
+					findIt = true;
+					lua_pop(L, 1);
+					break;
+				}
+
+				lua_pushstring(L, subClass.CString());
+				lua_rawget(L, -2);
+				if (lua_isnil(L, -1))
+				{
+					lua_pop(L, 1);
+					break;
+				}
+				subClass = lua_tostring(L, -1);
+				lua_pop(L, 1);
+			}
+
+			lua_pop(L, 1);
+			return findIt;
+		}
+
+		int luaex_pushusertype(lua_State* L, const char* userType, void* userValue)
+		{
+			if (!userValue)
+			{
+				lua_pushnil(L);
+				return 0;
+			}
+
+			luaex_pushusertypetable(L);
+
+			// 获取UserTable
+			lua_pushlightuserdata(L, userValue);
+			lua_rawget(L, -2);
+
+			if (lua_istable(L, -1))
+			{
+				lua_pushliteral(L, "__cname");
+				lua_rawget(L, -2);
+				const char* name = lua_tostring(L, -1);
+				lua_pop(L, 1);
+
+				// 判断类型一致，或者存在继承关系
+				if (strcmp(name, userType) == 0 || luaex_juedge_inherit(L, name, userType) || luaex_juedge_inherit(L, userType, name))
+				{
+#ifdef _DEBUG
+					// check 一下
+					lua_pushliteral(L, "__cinstance");
+					lua_rawget(L, -2);
+					void* cinstance = lua_touserdata(L, -1);
+					CRY_ASSERT(!cinstance || cinstance == userValue);
+					lua_pop(L, 1);
+#endif
+					// 移除userdata table
+					lua_remove(L, -2);
+					return 0;
+				}
+
+				// 这种情况因该是指针地址重用了，这时候lua保存的指针在C++中已经析构，直接设置成空指针就好了，之后覆盖原有的userdata
+				FLAGGG_LOG_STD_WARN("Export lua: address multi used, ptr:[%p] last type: [%s] this type:[%s]",
+					userValue, name, userType);
+				lua_pushliteral(L, "__cinstance");
+				lua_pushlightuserdata(L, nullptr);
+				lua_rawset(L, -3);
+			}
+
+			lua_pop(L, 1);
+			luaex_pushcontext(L);
+			// 新建UserTable
+			lua_newtable(L);  // @newtable
+			// 获取class表
+			lua_pushstring(L, userType);
+			lua_rawget(L, -3);
+			CRY_ASSERT(lua_istable(L, -1));
+
+			lua_getmetatable(L, -1);  // -1 is luaContext[userType]
+			lua_setmetatable(L, -3);  // -3 is @newtable, setmetatable(@newtable, luaContext[userType])
+
+			lua_pop(L, 1);  // pop luaContext[userType]
+			lua_remove(L, -2); // remove luaContext
+
+			lua_pushliteral(L, "__cname");
+			lua_pushstring(L, userType);
+			lua_rawset(L, -3);  // @newtable["__cname"] = userType
+
+			lua_pushliteral(L, "__cinstance");
+			lua_pushlightuserdata(L, userValue);
+			lua_rawset(L, -3);  // @newtable["__cinstance"] = &userValue
+
+			lua_pushlightuserdata(L, userValue);
+			lua_pushvalue(L, -2);  // push @newtable
+			lua_rawset(L, -4);     // LuaexUserData[&userValue] = @newtable
+
+			// 移除userdata table
+			lua_remove(L, -2);  // remove LuaexUserData
+
+			// return @newtable
+			return 1;
+		}
+
+		int luaex_pushusertyperef(lua_State* L, const char* userType, Container::RefCounted* ref)
+		{
+			int ret = luaex_pushusertype(L, userType, ref);
+			if (ret != 0)
+			{
+				ref->AddRef();
+			}
+			return ret;
+		}
+
+		void* luaex_tousertype(lua_State* L, int idx, const char* userType)
+		{
+#ifdef _DEBUG
+			// check一下
+			lua_pushliteral(L, "__cname");
+			lua_rawget(L, idx > 0 ? idx : idx - 1);
+			Container::String type = lua_tostring(L, -1);
+			lua_pop(L, 1);
+
+			if (!luaex_juedge_inherit(L, type, userType))
+				return nullptr;
+#endif
+			lua_pushliteral(L, "__cinstance");
+			lua_rawget(L, idx > 0 ? idx : idx - 1);
+			void* userValue = lua_touserdata(L, -1);
+			lua_pop(L, 1);
+			return userValue;
+		}
+	}
+}
